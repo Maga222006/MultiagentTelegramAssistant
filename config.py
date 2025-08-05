0c91fb6bb092edc134fafeb571971713748f76a7
Maga222006 <maga222006@mail.ru>
2025-08-06 02:04:06 +0300

    SQLlite database

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
-TOKEN = "Your Telegram Bot Token"
-DATABASE_URL = "Your Database URL"
+
+TOKEN = "7456008559:AAFbid5x8hhM4qe70SW9xyDHaiDCwHjkQH4"
+DATABASE_URL = "sqlite+aiosqlite:///./database/main.db"
